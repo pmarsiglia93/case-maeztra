2244d9adfbb3c9cf9814ad11c4dab3f3ce544fcc
Paulo Marsiglia <pmarsiglia93@gmail.com>
2025-02-19 16:24:56 -0300

    ajustes

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { HeaderDesktop, HeaderMobile } from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import useIsMobile from "./hooks/device";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 import "./styles/global.scss";
 
